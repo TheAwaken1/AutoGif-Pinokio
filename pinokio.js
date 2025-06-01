797755396362f5dcddedf9b5a10a318ea1ebbbe4
TheAwaken1 <cmpcreativedesigns@gmail.com>
2025-05-31 22:18:09 -0700

    Update pinokio.js

--- pinokio.js
+++ pinokio.js
@@ -1,101 +1,92 @@
-const path = require('path')
+const path = require('path');
 module.exports = {
-  version: "3.7",
-  title: "AutoGif-Pinokio",
-  description: "",
+  version: "2.0",
+  title: "🎬 AutoGif",
+  description: "Transform YouTube videos into stunning animated GIFs with perfectly-timed, stylized subtitles and eye-catching effects.",
   icon: "icon.png",
   menu: async (kernel, info) => {
-    let installed = info.exists("env")
+    let installed = info.exists("app/env");
     let running = {
       install: info.running("install.js"),
       start: info.running("start.js"),
       update: info.running("update.js"),
-      reset: info.running("reset.js"),
-      link: info.running("link.js")
-    }
+      reset: info.running("reset.js")
+    };
+
     if (running.install) {
       return [{
         default: true,
-        icon: "fa-solid fa-plug",
-        text: "Installing",
-        href: "install.js",
-      }]
+        icon: "fa-solid fa-download",
+        text: "Installing...",
+        href: "install.js"
+      }];
     } else if (installed) {
       if (running.start) {
-        let local = info.local("start.js")
+        let local = info.local("start.js");
         if (local && local.url) {
           return [{
             default: true,
             icon: "fa-solid fa-rocket",
             text: "Open Web UI",
-            href: local.url,
+            href: local.url
           }, {
-            icon: 'fa-solid fa-terminal',
+            icon: "fa-solid fa-terminal",
             text: "Terminal",
-            href: "start.js",
-          }]
+            href: "start.js"
+          }];
         } else {
           return [{
             default: true,
-            icon: 'fa-solid fa-terminal',
-            text: "Terminal",
-            href: "start.js",
-          }]
+            icon: "fa-solid fa-terminal",
+            text: "Starting...",
+            href: "start.js"
+          }];
         }
       } else if (running.update) {
         return [{
           default: true,
-          icon: 'fa-solid fa-terminal',
-          text: "Updating",
-          href: "update.js",
-        }]
+          icon: "fa-solid fa-sync",
+          text: "Updating...",
+          href: "update.js"
+        }];
       } else if (running.reset) {
         return [{
           default: true,
-          icon: 'fa-solid fa-terminal',
-          text: "Resetting",
-          href: "reset.js",
-        }]
-      } else if (running.link) {
-        return [{
-          default: true,
-          icon: 'fa-solid fa-terminal',
-          text: "Deduplicating",
-          href: "link.js",
-        }]
+          icon: "fa-solid fa-undo",
+          text: "Resetting...",
+          href: "reset.js"
+        }];
       } else {
         return [{
-          default: true,
-          icon: "fa-solid fa-power-off",
+          icon: "fa-solid fa-play",
           text: "Start",
-          href: "start.js",
+          href: "start.js"
         }, {
-          icon: "fa-solid fa-plug",
+          icon: "fa-solid fa-folder-open",
+          text: "View Output",
+          href: "app/output",
+          fs: true
+        }, {
+          icon: "fa-solid fa-sync",
           text: "Update",
-          href: "update.js",
+          href: "update.js"
         }, {
-          icon: "fa-solid fa-plug",
-          text: "Install",
-          href: "install.js",
+          icon: "fa-solid fa-download",
+          text: "Reinstall",
+          href: "install.js"
         }, {
-          icon: "fa-solid fa-file-zipper",
-          text: "<div><strong>Save Disk Space</strong><div>Deduplicates redundant library files</div></div>",
-          href: "link.js",
-        }, {
-          icon: "fa-regular fa-circle-xmark",
-          text: "<div><strong>Reset</strong><div>Revert to pre-install state</div></div>",
-          href: "reset.js",
-          confirm: "Are you sure you wish to reset the app?"
-
-        }]
+          icon: "fa-solid fa-trash",
+          text: "Reset",
+          href: "reset.js"
+        }];
       }
     } else {
       return [{
         default: true,
-        icon: "fa-solid fa-plug",
+        icon: "fa-solid fa-download",
         text: "Install",
-        href: "install.js",
-      }]
+        href: "install.js"
+      }];
     }
   }
 }
